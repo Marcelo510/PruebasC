f17303e24f19b7b5fc098f7e1d0cdbcd0aa1ef0f
Marcelo <Marcelo@MARCELO-NB>
2021-04-25 22:00:05 -0300

    Commit de arranque

---
+++ WebAppMVCCliente/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppMVCCliente.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
