d2fce249631a1af727ca1869ee1d29b3288b6c51
Marcelo <Marcelo@MARCELO-NB>
2021-04-26 11:23:30 -0300

    Commit al 26/04/2021

---
+++ WebAppReact/ClientApp/build/precache-manifest.5a74e3d50b3162c97dad8b49c3deaa6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecbd180f133cb1231804aee5d3734be6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf0c0dbe4c53e813d33d",
+    "url": "/static/css/2.56e33495.chunk.css"
+  },
+  {
+    "revision": "43f5175a06cfb170420a",
+    "url": "/static/css/main.13b298a8.chunk.css"
+  },
+  {
+    "revision": "bf0c0dbe4c53e813d33d",
+    "url": "/static/js/2.402e20ff.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.402e20ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43f5175a06cfb170420a",
+    "url": "/static/js/main.b6a0cccc.chunk.js"
+  },
+  {
+    "revision": "2ddcc3050659fda778e9",
+    "url": "/static/js/runtime-main.9e2a350c.js"
+  }
+]);
