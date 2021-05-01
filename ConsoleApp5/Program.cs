121b5c4d87cef4cf0130c7b168dac61dd551ee75
Marcelo <Marcelo@MARCELO-NB>
2021-05-01 15:23:09 -0300

    Commit

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            dynamic a = "Marcelo";
+            dynamic b = "otro";
+
+            a = 1;
+            
+            Console.WriteLine(a + b);
+
+
+
+        }
+
+        public void GeneraList()
+        {
+            var LaLista = new List<(int numero, string cadena)>
+            {
+                (1, "Marcelo"),
+                (2, "Alonso")
+            };
+
+
+            
+
+
+
+        }
+    }
+}
