bd09ae9a36a3c0a6611cde2217ea4b852251fd7a
Marcelo <Marcelo@MARCELO-NB>
2021-08-21 22:18:23 -0300

    Commit 1

--- ConsoleAppNet6/Program.cs
+++ ConsoleAppNet6/Program.cs
@@ -1,11 +1,15 @@
 ﻿
 
 
-var medios = new[] { "cheques", "bonos", "acciones", 1000, "transferencia" };
+using System.Collections;
+
+//var medios = new[] { "cheques", "bonos", "acciones", 1000, "transferencia" };
 
 
-var medios2 = {"cheques", "bonos", "acciones", 1000, "transferencia"};
+//var medios2 = {"cheques", "bonos", "acciones", 1000, "transferencia"};
 
+ArrayList arraylist = new ArrayList() { "cheques", "bonos", "acciones", 1000, "transferencia" };
 
-
-Console.WriteLine("Hello, World!");
+var elre = arraylist[1];
+var otro = elre[2];
+Console.WriteLine(elre);
